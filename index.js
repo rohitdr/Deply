e3eed8d236dd7e893a0b5d29b668816e7267e450
rohitdr <85736210+rohitdr@users.noreply.github.com>
2023-03-23 11:03:17 +0530

    nothin

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ app.use('/api/recipe',require('./Routes/Recipe.js'))
 
 //serving the indexfile
 if(process.env.NODE_ENV==='production'){
-  app.use(express.static(path.resolve(__dirname,'Frontend','build')))
+  app.use(express.static(path.join(__dirname,'./Frontend/build')))
   app.get('/',(req,res)=>{
 
-    res.sendFile(path.resolve(__dirname,'Frontend','build','index.html'))
+    res.sendFile(path.join(__dirname,"./Frontend/build/index.html"))
   })
 }
 
